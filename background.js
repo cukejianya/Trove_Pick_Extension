39d0b54633ea8c964d1328c7862ad82e2b49b002
Chinedum Mac <cukejianya@gmail.com>
2015-06-01 03:17:42 -0400

    able to convert bookmarklet to a url and use that in extension

---
+++ background.js
@@ -0,0 +1,5 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+  var action_url = "javascript:(function(d,id){var js,fjs=d.getElementsByTagName('head')[0];js=d.createElement('script');js.id=id;js.async=true;js['data-env']='//trove.com';js.src='//d2pe20ur0h0p8p.cloudfront.net/fbt/vendor/editorial/curation_init.js?cb='+new Date().getTime();fjs.parentNode.insertBefore(js,fjs);})(document,'trove_pick_js');
+";
+  chrome.tabs.update(tab.id, {url: action_url});
+});
